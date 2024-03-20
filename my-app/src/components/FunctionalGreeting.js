16782a5b3c934aa237199d2cfbafdf0d2c8ad968
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-03-20 15:45:42 +0000

    Create component using arrow function

--- my-app/src/components/FunctionalGreeting.js
+++ my-app/src/components/FunctionalGreeting.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+// function FunctionalGreeting() {
+//     return <h1>Hello from React</h1>
+// }
 
-function FunctionalGreeting() {
-    return <h1>Hello from React</h1>
-}
+// Writing Component using Arrow Function
+const FunctionalGreeting = () => <h1>Hello from React!</h1>;
 
 export default FunctionalGreeting;
