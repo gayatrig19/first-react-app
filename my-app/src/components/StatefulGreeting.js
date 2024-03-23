22b0536ae34de9372db37f2442728d1418cb7bc8
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-03-23 00:21:35 +0000

    Add a state to a class component

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,8 +1,26 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            // Initial state, represents the initial values of anything
+            // the component might change either on it's own or due to user interaction.
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
+    
     render () {
-        return <h1>Hello {this.props.name}, {this.props.greeting}</h1>;
+        return (
+            <div>
+                 <h1>{this.state.introduction} {this.props.name}, {this.props.greeting}</h1>
+                 <button>{this.state.buttonText}</button>
+            </div>
+           
+        )    
     }
 }
 
