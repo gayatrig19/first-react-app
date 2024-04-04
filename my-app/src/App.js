8c33079815e99f6bfa95555cc58f100cc891feb9
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-04-04 23:21:00 +0000

    Forms with Controlled Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,6 +16,7 @@ import RenderingLists from './components/RenderingLists';
 import LifeCyclesCDM from './components/LifeCyclesCDM';
 import LifeCyclesCDU from './components/LifeCyclesCDU';
 import LifeCyclesCWU from './components/LifeCyclesCWU';
+import ControlledForm from './components/ControlledForm';
 
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
       <LifeCyclesCDM />
       <LifeCyclesCDU />
       <LifeCyclesCWU />
+      <ControlledForm />
     </div>
   );
 }
---
+++ my-app/src/components/ControlledForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+export class ControlledForm extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: '',
+        }
+    }
+
+    handleNameChange = (event) => {
+        //let newValue = event.target.value
+        //console.log(newValue);
+        this.setState ({  
+            name: event.target.value,
+        });   
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <label htmlFor="id-name">Your Name:</label>
+                    <input
+                        onChange={this.handleNameChange}
+                        value={this.state.name}
+                        id="id-name" name="name"
+                        type="text"
+                    />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default ControlledForm;
